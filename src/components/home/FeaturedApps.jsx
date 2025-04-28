15c8e1a672f1be70f598758661ad28d52945f8ed
SirBoltzmannm <dario24006@gmail.com>
2025-04-28 18:20:21 -0500

    feat: enhaced responsive structure of -featured applications- section in home page, and changed hero-img for a better visual experience

--- src/components/home/FeaturedApps.jsx
+++ src/components/home/FeaturedApps.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import './FeaturedApps.css';
-import PlanetTrajectory from '../../assets/planet-trajectory.png';
-import Exoplanet from '../../assets/exo-stars.png';
+import Planet from '../../assets/planet.png';
+import Constellations from '../../assets/constellation.png';
 import Moon from '../../assets/moon.png';
 
 
@@ -9,34 +9,36 @@ const FeaturedApps = () => {
     return (
         <div className='featured-apps'>
             {/* feat-apps stands for featured-apps */}
-            <h2 className="feat-apps-subheading">Featured Applications</h2>
+            <div className="featured-apps-container">
+                <h2 className="feat-apps-subheading">Featured Applications</h2>
 
-            <div className="feat-apps-wrapper">
-                <div className="stellar-trajectories-app featured-app">
-                    <Link to={'/stellar-trajectories'}>
-                        <img className='planet-trajectory' src={PlanetTrajectory} alt="planet-img" />
-                    </Link>
-                    <Link to={'/stellar-trajectories'}>
-                        <span className='app-title'>Stellar Trajectories</span>
-                    </Link>
-                </div>
+                <div className="feat-apps-wrapper">
+                    <div className="stellar-trajectories-app featured-app">
+                        <Link to={'/stellar-trajectories'}>
+                            <img className='planet-trajectory' src={Planet} alt="planet-img" />
+                        </Link>
+                        <Link to={'/stellar-trajectories'}>
+                            <span className='app-title'>Stellar Trajectories</span>
+                        </Link>
+                    </div>
 
-                <div className="stars-and-exoplanets-app featured-app">
-                    <Link to={'/stars-planets'}>
-                        <img className='exoplanet' src={Exoplanet} alt="exoplanet-img" />
-                    </Link>
-                    <Link to={'/stars-planets'}>
-                        <span className='app-title'>Stars and Exoplanets</span>
-                    </Link>
-                </div>
+                    <div className="stars-and-constellations-app featured-app">
+                        <Link to={'/stars-planets'}>
+                            <img className='constellation' src={Constellations} alt="constellation-img" />
+                        </Link>
+                        <Link to={'/stars-planets'}>
+                            <span className='app-title'>Stars and Exoplanets</span>
+                        </Link>
+                    </div>
 
-                <div className="sun-and-moon-app featured-app">
-                    <Link to={'/sun-moon'}>
-                        <img className='moon' src={Moon} alt="moon-img" />
-                    </Link>
-                    <Link to={'/sun-moon'}>
-                        <span className='app-title'>Sun and Moon</span>
-                    </Link>
+                    <div className="sun-and-moon-app featured-app">
+                        <Link to={'/sun-moon'}>
+                            <img className='moon' src={Moon} alt="moon-img" />
+                        </Link>
+                        <Link to={'/sun-moon'}>
+                            <span className='app-title'>Sun and Moon</span>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
